3df2a985bef2c2298da61c6e2f6a850b481834c3
Pravesh Kumar <praveshkumar1062002@gmail.com>
2023-05-20 11:53:11 +0530

    css updation 2

--- controller/productController.js
+++ controller/productController.js
@@ -217,7 +217,7 @@ export const prdouctCountController=async(req,res)=>{
 // product list based on page
 export const prdouctListController=async(req,res)=>{
   try {
-      const perPage=6;
+      const perPage=4;
       const page=req.params?req.params.page:1
       const products=await productModel.find({}).select("-photo").skip((page-1)*(perPage)).limit(perPage).sort({createdAt:-1});
       res.status(200).send({
