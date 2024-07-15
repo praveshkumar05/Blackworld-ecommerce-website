397a705c45c92ac65cd2971b518015c918d76624
Pravesh Kumar <praveshkumar1062002@gmail.com>
2024-07-15 10:42:45 +0530

    backend connection string is modified

--- client/src/components/Layout.js/APIS/baseurl.js
+++ client/src/components/Layout.js/APIS/baseurl.js
@@ -1,4 +1,4 @@
-//  export const BASE_URL="http://localhost:6010/api/v1"
+export const BASE_URL="http://localhost:6010/api/v1"
 // https://pink-tasty-lion.cyclic.app/
 //  export const BASE_URL="https://pink-tasty-lion.cyclic.app/api/v1"
-  export const BASE_URL="https://blackworld-ecommerce-website.vercel.app/"
+//   export const BASE_URL="https://blackworld-ecommerce-website.vercel.app/"
