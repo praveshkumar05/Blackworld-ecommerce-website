cd333ca6407069c5e030d3b5c0e632c55cf99f3d
Pravesh Kumar <praveshkumar1062002@gmail.com>
2024-08-03 14:02:21 +0530

    updated the .vercelingore file by ignoring the client side

--- client/src/components/Layout.js/APIS/baseurl.js
+++ client/src/components/Layout.js/APIS/baseurl.js
@@ -1,3 +1,3 @@
-export const BASE_URL="http://localhost:6010/api/v1"
+export const BASE_URL="https://blackworld-ecommerce-website-ahmruw25o-20ucs022s-projects.vercel.app/api/v1"
 // export const BASE_URL="https://pink-tasty-lion.cyclic.app/api/v1"
 // export const BASE_URL="https://blackworld-ecommerce-website.vercel.app/api/v1"
